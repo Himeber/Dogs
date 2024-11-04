b4be1887e73a460e1f5367240f95c1d6f32a8aa0
George Busby <georgebusby08@gmail.com>
2024-11-04 15:12:20 -0700

    Update dog_class.py

--- dog_class.py
+++ dog_class.py
@@ -1,7 +1,8 @@
-class Dog:
+fclass Dog:
   def __init__(self,name,breed):
     self.name = name
     self.breed = breed
   def __repr__(self):
     return f"{self.name} - {self.breed}"
-  
+  def __str__(self):
+    return str(self.__repr__())
